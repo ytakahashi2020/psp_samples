2b6a1c12b40563f5589589a632638ffb779f629c
ytakahashi2020 <y.takahashi2020@gmail.com>
2023-08-03 13:41:05 +0900

    modify new function

--- psp34/lib.rs
+++ psp34/lib.rs
@@ -8,6 +8,7 @@ pub mod my_psp34 {
             metadata::*,
             mintable::*,
             burnable::*,
+            enumerable::*,
         },
         traits::{
             Storage,
@@ -19,7 +20,7 @@ pub mod my_psp34 {
     #[derive(Default, Storage)]
     pub struct Contract {
         #[storage_field]
-        psp34: psp34::Data,
+        psp34: psp34::Data<Balances>,
         #[storage_field]
         metadata: Data,
         next_id: u8,
@@ -31,27 +32,17 @@ pub mod my_psp34 {
 
     impl PSP34Burnable for Contract {}
 
+    impl PSP34Enumerable for Contract {}
+
     impl Contract {
         #[ink(constructor)]
         pub fn new() -> Self {
             let mut _instance = Self::default();
-			psp34::Internal::_mint_to(&mut _instance, Self::env().caller(), Id::U8(1)).expect("Can mint");
+			// psp34::Internal::_mint_to(&mut _instance, Self::env().caller(), Id::U8(1)).expect("Can mint");
 			let collection_id = _instance.collection_id();
 			metadata::Internal::_set_attribute(&mut _instance, collection_id.clone(), String::from("name"), String::from("MyPSP34"));
 			metadata::Internal::_set_attribute(&mut _instance, collection_id, String::from("symbol"), String::from("MPSP"));
 			_instance
         }
-
-        // #[ink(message)]
-        // pub fn mint_token(&mut self) -> Result<(), PSP34Error> {
-        //     self._mint_to(Self::env().caller(), Id::U8(self.next_id))?;
-        //     self.next_id += 1;
-        //     Ok(())
-        // }
-
-        // #[ink(message)]
-        // pub fn mint(&mut self, id: Id) -> Result<(), PSP34Error> {
-        //     self._mint_to(Self::env().caller(), id)
-        // }
     }
 }
