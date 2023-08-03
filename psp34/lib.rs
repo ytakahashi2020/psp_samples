3f27af8dc2a359bfd4bf807cf3bdc4a03d0c5d67
ytakahashi2020 <y.takahashi2020@gmail.com>
2023-08-03 13:26:23 +0900

    first commit

---
+++ psp34/lib.rs
@@ -0,0 +1,45 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+#![feature(min_specialization)]
+
+#[openbrush::contract]
+pub mod my_psp34 {
+    use openbrush::{
+        contracts::psp34::extensions::{
+            metadata::*,
+            mintable::*,
+        },
+        traits::Storage,
+    };
+
+    #[ink(storage)]
+    #[derive(Default, Storage)]
+    pub struct Contract {
+        #[storage_field]
+        psp34: psp34::Data,
+        #[storage_field]
+        metadata: Data,
+        next_id: u8,
+    }
+
+    impl PSP34 for Contract {}
+
+    impl Contract {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            let mut _instance = Self::default();
+			_instance
+        }
+
+        #[ink(message)]
+        pub fn mint_token(&mut self) -> Result<(), PSP34Error> {
+            self._mint_to(Self::env().caller(), Id::U8(self.next_id))?;
+            self.next_id += 1;
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn mint(&mut self, id: Id) -> Result<(), PSP34Error> {
+            self._mint_to(Self::env().caller(), id)
+        }
+    }
+}
